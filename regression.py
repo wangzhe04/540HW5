88021e38bee8297f833cd5cb2595c9529f4caa07
wangzhe04 <67325944+wangzhe04@users.noreply.github.com>
2021-03-08 14:49:04 +0800

    Add files via upload

--- regression.py
+++ regression.py
@@ -83,8 +83,9 @@ def regression(dataset, cols, betas):
         mse of the regression model mean square error
     """
     # sperate b0 from betas
-    b0 = betas[0]
-    betas.pop(0)
+    new_betas = betas.copy()
+    b0 = new_betas[0]
+    new_betas.pop(0)
 
     sum = 0
     num = 0
@@ -92,7 +93,7 @@ def regression(dataset, cols, betas):
     for i in dataset:
         model = b0
         a = 0
-        for k in betas:
+        for k in new_betas:
             # add bi * xi to model
             model += float(k) * float(i[int(cols[a])])
             a += 1
@@ -108,38 +109,34 @@ def regression(dataset, cols, betas):
 def gradient_descent(dataset, cols, betas):
     """
     TODO: implement this function.
-
-    INPUT: 
+    INPUT:
         dataset - the body fat n by m+1 array
         cols    - a list of feature indices to learn.
                   For example, [1,8] refers to density and abdomen.
         betas   - a list of elements chosen from [beta0, beta1, ..., betam]
-
     RETURNS:
         An 1D array of gradients
     """
-    b0 = betas[0]
-    betas.pop(0)
+    new_betas = betas.copy()
+
+    # sperate the betas[0] from new_betas
+    b0 = new_betas[0]
+    new_betas.pop(0)
 
     sum = 0
     num = 0
-    sum2 = 0
 
     my_vars = {}
-    for i in range(len(betas)+1):
-        var_name = "var%d" % i
-        my_vars[i] = 0
-    # print(my_vars)
 
     list1 = []
-    for i in range(len(betas)):
+    for i in range(len(new_betas)):
         list1.append(0)
 
 
     for i in dataset:
         model = b0
         a = 0
-        for k in betas:
+        for k in new_betas:
             # add bi * xi to model
             model += float(k) * float(i[int(cols[a])])
             a += 1
@@ -147,7 +144,7 @@ def gradient_descent(dataset, cols, betas):
         # add the value of the formula to the total sum
 
 
-        for z in range(len(betas) ):
+        for z in range(len(new_betas) ):
 
             list1[z] += (model - float(i[0])) * float(i[int(cols[z])])
             # print(z)
@@ -158,17 +155,13 @@ def gradient_descent(dataset, cols, betas):
 
 #    print(list1)
     list = []
-    for z in range(len(betas)):
+    for z in range(len(new_betas)):
         list1[z] = list1[z] * 2/num
 
     list.append(sum*2/num)
     list = list + list1
     # print(sum2*2/num)
     b = 0
-#    for
-
-
-
 
     grads = list
     return grads
@@ -189,6 +182,18 @@ def iterate_gradient(dataset, cols, betas, T, eta):
     RETURNS:
         None
     """
+    new_betas = betas.copy()
+
+    for i in range(T):
+        # print(i+1)
+        # print(betas)
+        list =[]
+        for k in range(len(new_betas)):
+            new_betas[k] = new_betas[k] - (gradient_descent(dataset, cols, new_betas)[k])*eta
+            list.append(new_betas[k])
+
+        print(i+1, regression(dataset,cols,new_betas), list)
+        # print(gradient_descent(dataset,cols,betas))
     pass
 
 
@@ -256,8 +261,10 @@ if __name__ == '__main__':
     dataset = get_dataset('bodyfat.csv')
     # print_stats(get_dataset('bodyfat.csv'), 1)
     print(regression(dataset, cols=[2,3], betas=[0,0,0]))
-    # print('')
     print(regression(dataset, cols=[2,3,4], betas=[0,-1.1,-.2,3]))
+    # print('')
+    # print(regression(dataset, cols=[2,3,4], betas=[0,-1.1,-.2,3]))
     print(gradient_descent(dataset, cols=[2,3], betas=[0,0,0]))
+    iterate_gradient(dataset, cols=[1,8], betas=[400,-400,300], T=10, eta=1e-4)
     ### DO NOT CHANGE THIS SECTION ###
     plot_mse()
